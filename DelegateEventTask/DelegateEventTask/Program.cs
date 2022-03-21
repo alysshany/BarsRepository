3dbb9d900de63769f820a71c5dec19ac357a0fa1
g.zilya <galievaaziz@gmail.com>
2022-03-21 23:01:03 +0300

    added first task

---
+++ DelegateEventTask/DelegateEventTask/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace DelegateEventTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EventClass events = new EventClass();
+            events.OnKeyPressed += Handler;
+            events.Run();
+        }
+
+        public static void Handler(object? sender, char charKey)
+        {
+            Console.WriteLine($"Your char: {charKey}");
+        }
+    }
+
+    public class EventClass
+    {
+        public event EventHandler<char> OnKeyPressed;
+
+        public void Run()
+        {
+            bool going = true;
+            while (going)
+            {
+                Console.WriteLine("Press the key:");
+                var letter = Convert.ToChar(Console.ReadLine());
+
+                if (letter == 'c')
+                {
+                    going = false;
+                }
+                else
+                {
+                    OnKeyPressed?.Invoke(this, letter);
+                }
+            }
+        }
+    }
+}
